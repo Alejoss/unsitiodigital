437a90a1edb6daccce2070b7bb776f93033deadd
Alejoss <alejoveintimilla@gmail.com>
2015-06-01 15:34:44 -0500

    imagenes extra

--- unsitiodigital/settings.py
+++ unsitiodigital/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ['USDIGITAL_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-HEROKU = True
+HEROKU = False
 
 ALLOWED_HOSTS = ["*"]
 
